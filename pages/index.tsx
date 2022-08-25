170ac37a81d37066545de461887db792030653ce
Eric Echemane <eechemane29@gmail.com>
2022-08-25 21:59:38 +0800

    added geist icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import { Button } from '@geist-ui/core';
-import { useThemeChangerContext } from 'contexts/theme_changer';
+import { Button, Text } from '@geist-ui/core';
 import React from 'react';
 
 export default function Home() {
-  const { switchThemes } = useThemeChangerContext();
-
   return <>
-    <h1> Eric Echemane </h1>
-    <Button margin={2} onClick={switchThemes}> Toggle Theme </Button>
+    <Text blockquote> My Belove Son </Text>
+    <Button type='secondary-light'> Secondary </Button>
   </>;
 }
